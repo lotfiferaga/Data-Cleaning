807e7d03b2bf028916cbf1a444e5686b254828bb
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2023-12-07 10:01:30 +0100

    Update Data-Cleaning.py

--- Data-Cleaning.py
+++ Data-Cleaning.py
@@ -1,1 +1,16 @@
+# importing work libraries
+import pandas as pd
+import numpy as np
+
+
+# reading the dataset
+df = pd.read_csv("heart.csv")
+
+# displaying the first 10 rows of the dataset
+df.head(10)
+
+# last 10 rows
+df.tail(10)
+
+
 
